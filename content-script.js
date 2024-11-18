a43bce6c6a809939c4e41e3e0f485db4052c6c03
Gino Van Houdt <gino.vanhoudt@lab900.com>
2024-11-18 10:12:03 +0100

    chip-test color

--- content-script.js
+++ content-script.js
@@ -1,5 +1,9 @@
 const STIP_DEV_URL = 'stip-dev.seatankterminal.com';
 const STIP_TEST_URL = 'stip-test.seatankterminal.com';
+
+const CHIP_DEV_URL = 'chip-dev.seatankterminal.com';
+const CHIP_TEST_URL = 'chip-test.seatankterminal.com';
+
 const STIP_PROD_URL = 'stip.seatankterminal.com';
 
 // html EL is not immediately available, so we need to wait for it
@@ -11,28 +15,39 @@ function onElementAvailable(className, callback) {
       callback(el);
     }
   });
-  observer.observe(document.body, { childList: true, subtree: true });
+  observer.observe(document.body, {childList: true, subtree: true});
 }
 
 let currentUrl = document.location.href;
 
 let env;
 if (currentUrl.includes(STIP_DEV_URL)) {
-  env = 'dev';
+  env = 'stip-dev';
 } else if (currentUrl.includes(STIP_TEST_URL)) {
-  env = 'test';
+  env = 'stip-test';
 } else if (currentUrl.includes(STIP_PROD_URL)) {
   env = 'prod';
+} else if (currentUrl.includes(CHIP_DEV_URL)) {
+  env = 'chip-dev';
+} else if (currentUrl.includes(CHIP_TEST_URL)) {
+  env = 'chip-test';
 }
 
 if (env) {
   onElementAvailable('core-header', (el) => {
-    el.classList.add(`stip-${env}-color`);
+    el.classList.add(`${env}-color`);
 
     const divElement = document.createElement("div");
     const textNode = document.createTextNode(`${env.toUpperCase()} ENVIRONMENT`);
     divElement.appendChild(textNode);
     divElement.classList.add('white-text');
     el.children?.[0].appendChild(divElement);
+  });
+}
+
+// add dark mode
+if (env == null) {
+  onElementAvailable('mat-typography', (el) => {
+    el.classList.add('dark-mode');
   });
 }
